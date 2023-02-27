f18e4f94c6da68e7ef073a3e6a055a637fa048f1
DarioGiusino <dario.giusino@gmail.com>
2023-02-27 15:23:08 +0100

    defined table row in seeder using faker

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -2,16 +2,32 @@
 
 namespace Database\Seeders;
 
+use App\Models\Train;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class TrainSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+            $new_train = new Train();
+
+            $new_train->company = $faker->company();
+            $new_train->departure_station = $faker->words(2, true);
+            $new_train->arrival_station = $faker->words(2, true);
+            $new_train->departure_time = $faker->time();
+            $new_train->arrival_time = $faker->time();
+            $new_train->trian_code = $faker->randomNumber(5, false);
+            $new_train->carriages_number = $faker->randomNumber(3, false);
+            $new_train->in_time = $faker->boolean();
+            $new_train->is_cancelled = $faker->boolean();
+
+            $new_train->save();
+        }
     }
 }
