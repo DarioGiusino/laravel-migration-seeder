4c0a86dfdd597a44dab1fb15304e45ab1c3a353a
DarioGiusino <dario.giusino@gmail.com>
2023-02-27 14:37:04 +0100

    hard coded example row in table

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,17 +15,29 @@
     <thead>
       <tr>
         <th scope="col">#</th>
-        <th scope="col">First</th>
-        <th scope="col">Last</th>
-        <th scope="col">Handle</th>
+        <th scope="col">Azienda</th>
+        <th scope="col">Stazione di partenza</th>
+        <th scope="col">Stazione di arrivo</th>
+        <th scope="col">Orario di partenza</th>
+        <th scope="col">Oraio di arrivo</th>
+        <th scope="col">Codice Treno</th>
+        <th scope="col">Numero Carrozze</th>
+        <th scope="col">In orario</th>
+        <th scope="col">Cancellato</th>
       </tr>
     </thead>
     <tbody>
       <tr>
         <th scope="row">1</th>
-        <td>Mark</td>
-        <td>Otto</td>
-        <td>@mdo</td>
+        <td>Trenitalia</td>
+        <td>Stazione notarbartolo</td>
+        <td>stazione centrale</td>
+        <td>10:30</td>
+        <td>12:30</td>
+        <td>961273</td>
+        <td>17</td>
+        <td>Mai</td>
+        <td>può essere</td>
       </tr>
     </tbody>
   </table>
