3e544b40c8bcbd408f79cfacea970b390b29c32c
DarioGiusino <dario.giusino@gmail.com>
2023-02-27 14:08:42 +0100

    changed to bs 5.3

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,10 @@
+{{-- extends main layout --}}
 @extends('layouts.main')
 
+{{-- chaging theme to dark --}}
+@section('theme')
+  data-bs-theme="dark"
+@endsection
+
+{{-- changing title --}}
 @section('title', 'Home')
