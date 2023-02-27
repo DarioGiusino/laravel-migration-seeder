dc392e78ca3b9a95d902bc513d3615f4e65d5425
DarioGiusino <dario.giusino@gmail.com>
2023-02-27 14:31:14 +0100

    insert an example table

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,24 @@
 {{-- changing title --}}
 @section('title', 'Home')
 
-{{-- main content --}}
+{{-- main content / train table --}}
 @section('content')
-
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">First</th>
+        <th scope="col">Last</th>
+        <th scope="col">Handle</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+      </tr>
+    </tbody>
+  </table>
 @endsection
